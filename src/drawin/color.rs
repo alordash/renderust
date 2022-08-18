f4d17b1529123eaedb520cdeb39eaede64f7f14e
alordash <alordash@mail.ru>
2022-08-18 21:53:22 +0300

    Added Color::from_hsv

--- src/drawin/color.rs
+++ src/drawin/color.rs
@@ -21,11 +21,35 @@ impl Color {
         }
     }
 
-    pub fn new(r: u8, g: u8, b: u8) -> Color {
+    pub fn from_rgb(r: u8, g: u8, b: u8) -> Color {
         Color { b, g, r, alpha: 0 }
     }
 
-    pub fn new_with_alpha(r: u8, g: u8, b: u8, alpha: u8) -> Color {
+    pub fn from_rgb_with_alpha(r: u8, g: u8, b: u8, alpha: u8) -> Color {
         Color { b, g, r, alpha }
+    }
+
+    pub fn from_hsv(h: u16, s: f32, v: f32) -> Color {
+        let c = v * s as f32;
+        let x = c * (1.0 - ((h as f32 / 60.0) % 2.0 - 1.0).abs()) as f32;
+        let m = v - c;
+
+        let (r0, g0, b0) = match h {
+            0..=59 => (c, x, 0.0),
+            60..=119 => (x, c, 0.0),
+            120..=179 => (0.0, c, x),
+            180..=239 => (0.0, x, c),
+            240..=299 => (x, 0.0, c),
+            300..=359 => (c, 0.0, x),
+            _ => (0.0, 0.0, 0.0),
+        };
+
+        let (r, g, b): (u8, u8, u8) = (
+            ((r0 + m) * 255.0) as u8,
+            ((g0 + m) * 255.0) as u8,
+            ((b0 + m) * 255.0) as u8,
+        );
+
+        Color::from_rgb(r, g, b)
     }
 }
