c5109601497056b2afedb94d01cf4083246ad6f5
alordash <alordash@mail.ru>
2022-08-19 01:25:52 +0300

    Pre-final optimaztions for line drawer

--- src/drawin/geometry_drawin/discrete_line_drawin.rs
+++ src/drawin/geometry_drawin/discrete_line_drawin.rs
@@ -10,25 +10,46 @@ impl Drawable for DiscreteLine {
 
         let mut dx = p2.x - p1.x;
         let mut dy = p2.y - p1.y;
-        let mut k = dy as f32 / dx as f32;
         let mut angle_bigger_45_deg = false;
 
-        if k.abs() > 1.0 {
+        if dy.abs() > dx.abs() {
             angle_bigger_45_deg = true;
             (p1.x, p1.y) = (p1.y, p1.x);
             (p2.x, p2.y) = (p2.y, p2.x);
-            k = 1.0 / k;
-            (dx, dy) = (dy, dx);
         }
 
         if p2.x < p1.x {
             (p1, p2) = (p2, p1);
         }
 
-        for x in p1.x..p2.x {
-            let y = (k * (x - p1.x) as f32) as isize + p1.y;
-            let (x0, y0) = if angle_bigger_45_deg { (y, x) } else { (x, y) };
-            canvas[(x0 as usize, y0 as usize)] = *color;
+        dx = p2.x - p1.x;
+        dy = p2.y - p1.y;
+
+        let dy_error = dy.abs() * 2;
+        let mut y_error = 0;
+        let mut y = p1.y as usize;
+
+        let iter_range = p1.x as usize..p2.x as usize;
+        for x in iter_range {
+            // This condition can be optimized
+            // by taking it out of loop
+            if angle_bigger_45_deg {
+                canvas[(y, x)] = *color;
+            } else {
+                canvas[(x, y)] = *color;
+            }
+
+            y_error += dy_error;
+            if y_error > dx {
+                // This condition can be optimized
+                // by taking it out of loop
+                if p2.y > p1.y {
+                    y += 1;
+                } else {
+                    y -= 1;
+                }
+                y_error -= dx * 2;
+            }
         }
     }
 }
